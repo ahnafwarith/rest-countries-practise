9716948b87842af7cd1f82080e86e1400cb90021
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-19 02:44:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+function LoadCountries() {
+  const [user, LoadUser] = useState();
+  useEffect(() => {
+
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+  }, []);
+  return (
+    <h1>I am loading from JSON rest countries</h1>
+  )
 }
 
 export default App;
